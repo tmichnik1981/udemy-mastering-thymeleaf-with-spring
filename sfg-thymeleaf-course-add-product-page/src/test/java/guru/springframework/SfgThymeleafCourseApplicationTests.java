d0d149df81d5b723bbfca8d393a744f997fff939
Tomasz Michnik <tom.michnik@gmail.com>
2018-04-05 22:48:58 +0200

    Updated code

--- sfg-thymeleaf-course-add-product-page/src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
+++ sfg-thymeleaf-course-add-product-page/src/test/java/guru/springframework/SfgThymeleafCourseApplicationTests.java
@@ -2,17 +2,17 @@ package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SfgThymeleafCourseApplication.class)
+@SpringApplicationConfiguration(classes = SfgThymeleafCourseApplication.class)
 @WebAppConfiguration
 public class SfgThymeleafCourseApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
